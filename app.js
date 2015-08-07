2c812e8a1bcbc0235a846f37befae72c6fd2a989
Amy Liu <amyjialiu2015@gmail.com>
2015-08-07 10:45:12 -0700

    modify app.js for highcharts path

--- app.js
+++ app.js
@@ -22,8 +22,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'bower_components')));
-
+app.use(express.static(path.join(__dirname, 'node_modules')));
 app.use('/', routes);
+
 app.use('/users', users);
 
 // catch 404 and forward to error handler
